af02dd12f1a34d37a4fa9f2bd438e4d8b6231e9c
gamberooni <zhaoyoong@gmail.com>
2021-06-07 20:43:14 +0800

    populate composite table titles_casts

--- airflow/dags/create_tables.py
+++ airflow/dags/create_tables.py
@@ -20,25 +20,12 @@ with DAG(
         sql="""
         CREATE TABLE IF NOT EXISTS dim_download_date (
             id SERIAL PRIMARY KEY,
-            year SMALLINT,
-            month SMALLINT,
-            day SMALLINT
+            year SMALLINT NOT NULL,
+            month SMALLINT NOT NULL,
+            day SMALLINT NOT NULL
         );
         """,
     )
-
-    create_dim_ratings_table = PostgresOperator(
-        task_id="create_dim_ratings_table",
-        postgres_conn_id="imdb_postgres",
-        sql="""
-        CREATE TABLE IF NOT EXISTS dim_ratings (
-            id SERIAL PRIMARY KEY,
-            tconst VARCHAR(15),
-            av_rating FLOAT(2),
-            num_votes INT
-        );
-        """,
-    )    
 
     create_dim_episode_table = PostgresOperator(
         task_id="create_dim_episode_table",
@@ -58,14 +45,19 @@ with DAG(
         sql="""
         CREATE TABLE IF NOT EXISTS dim_title_desc (
             id SERIAL PRIMARY KEY,
-            tconst VARCHAR(20),
+            tconst VARCHAR(20) UNIQUE NOT NULL,
             type VARCHAR(40),
-            primary_title VARCHAR(800),
+            primary_title VARCHAR(800) NOT NULL,
             original_title VARCHAR(800),
             is_adult BOOLEAN,
             start_year SMALLINT,
             end_year SMALLINT,
-            runtime_minutes SMALLINT
+            runtime_minutes SMALLINT,
+            av_rating FLOAT(2),
+            num_votes INT,
+            genre_1 VARCHAR(25),
+            genre_2 VARCHAR(25),
+            genre_3 VARCHAR(25)
         );
         """,
     )    
@@ -76,12 +68,11 @@ with DAG(
         sql="""
         CREATE TABLE IF NOT EXISTS dim_casts (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(200),
+            name VARCHAR(200) NOT NULL,
             character VARCHAR(300),
             age SMALLINT,
             is_alive BOOLEAN,
-            nconst VARCHAR(15),
-            tconst VARCHAR(15)
+            nconst VARCHAR(15) NOT NULL
         );
         """,
     )        
@@ -93,13 +84,10 @@ with DAG(
         CREATE TABLE IF NOT EXISTS fact_titles (
             id SERIAL PRIMARY KEY,
             downloadDate_id INT REFERENCES dim_download_date (id),
-            ratings_id INT REFERENCES dim_ratings (id),
             episode_id INT REFERENCES dim_episode (id),
-            titleDesc_id INT REFERENCES dim_title_desc (id),
-            titles_genres_id INT,
+            title_desc_id INT REFERENCES dim_title_desc (id),
             titles_casts_id INT,    
             titles_crew_id INT,                          
-            tconst VARCHAR(20) UNIQUE,
             daily_avRatingChange FLOAT(2),
             weekly_avRatingChange FLOAT(2),
             daily_numVotesChange INT,
@@ -107,28 +95,6 @@ with DAG(
         );
         """,
     )      
-
-    create_titles_genres_table = PostgresOperator(
-        task_id="create_titles_genres_table",
-        postgres_conn_id="imdb_postgres",
-        sql="""
-        CREATE TABLE IF NOT EXISTS titles_genres (
-            title_id INT,
-            genre_id INT REFERENCES dim_genres (id)
-        );
-        """,
-    )         
-
-    create_dim_genres_table = PostgresOperator(
-        task_id="create_dim_genres_table",
-        postgres_conn_id="imdb_postgres",
-        sql="""
-        CREATE TABLE IF NOT EXISTS dim_genres (
-            id SERIAL PRIMARY KEY,
-            genre VARCHAR(25)
-        );
-        """,
-    )     
 
     create_titles_crew_table = PostgresOperator(
         task_id="create_titles_crew_table",
@@ -147,12 +113,11 @@ with DAG(
         sql="""
         CREATE TABLE IF NOT EXISTS dim_crew (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(200),
-            role VARCHAR(50),
+            name VARCHAR(200) NOT NULL,
+            role VARCHAR(50) NOT NULL,
             age SMALLINT,
             is_alive BOOLEAN,
-            tconst VARCHAR(15),
-            nconst VARCHAR(15)
+            nconst VARCHAR(15) NOT NULL
         );
         """,
     )    
@@ -179,10 +144,6 @@ with DAG(
         ALTER TABLE titles_crew 
         ADD CONSTRAINT titles_crew_title_id_fkey FOREIGN KEY (title_id)
         REFERENCES fact_titles (id);
-
-        ALTER TABLE titles_genres 
-        ADD CONSTRAINT titles_genres_title_id_fkey FOREIGN KEY (title_id)
-        REFERENCES fact_titles (id);
         """,
     )                  
 
@@ -206,9 +167,7 @@ with DAG(
     create_schema >> create_dim_title_desc_table >> create_fact_titles_table
     create_schema >> create_dim_download_date_table >> create_fact_titles_table
     create_schema >> create_dim_episode_table >> create_fact_titles_table 
-    create_schema >> create_dim_genres_table >> create_titles_genres_table >> create_fact_titles_table
     create_schema >> create_dim_crew_table >> create_titles_crew_table >> create_fact_titles_table
     create_schema >> create_dim_casts_table >> create_titles_casts_table >> create_fact_titles_table
-    create_schema >> create_dim_ratings_table >> create_fact_titles_table
     
     create_fact_titles_table >> set_fkey
